78d853cd58fa3a5bcaeaa3b4962956013d489067
BrGondim <160553381+BrGondim@users.noreply.github.com>
2025-11-10 12:10:29 -0300

    Create build.sh
    Adiciona script do estoque

---
+++ estoque-service/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "=========================================="
+echo "INICIANDO BUILD DO 'Controle de Estoque'"
+echo "Executando testes simulados..."
+sleep 1
+echo "Build concluído com sucesso."
+echo "=========================================="
